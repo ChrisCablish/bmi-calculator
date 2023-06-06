89c3bf8129956362f61d17ac44293c2e68c6c269
Chris <chriscablish@hotmail.com>
2023-06-06 09:18:48 -0500

    fix result message grammar

--- script.js
+++ script.js
@@ -81,13 +81,13 @@ const calcWeightInLbs = () => {
 
 const classifyBMI = (bmi) => {
     if (bmi < 18.5) {
-        classification = 'underweight';
+        classification = 'underweight.';
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-        classification = 'normal Weight';
+        classification = 'normal weight.';
     } else if (bmi >= 25 && bmi <= 29.9) {
-        classification = 'overweight';
+        classification = 'overweight.';
     } else if (bmi > 30) {
-        classification = 'obese'
+        classification = 'obese.'
     }
 }
 
